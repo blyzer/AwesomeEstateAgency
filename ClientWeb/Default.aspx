4e46f6c3993f97b4189d74fcff9ca7a7dc0f10c2
benjaminchazelle <bchazelle.dev@outlook.fr>
2016-02-11 10:22:42 +0100

    initial upload

---
+++ ClientWeb/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/General.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ClientWeb.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:TextBox ID="TextBox1"  AutoPostBack="false" runat="server" />
+    <asp:Label ID="Label1"  runat="server" />
+    <asp:Button Text="Submit" runat="server" />
+
+</asp:Content>
---
+++ ClientWeb/Default.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ClientWeb
+{
+
+
+
+
+
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (this.IsPostBack)
+            {
+
+                // Label1.Text = "<b>" + TextBox1.Text.ToUpper() + "</b>";
+
+              //  List<ServiceAgence.BienImmobilier> liste = null;
+
+
+                using (ServiceAgence.AgenceClient client = new ServiceAgence.AgenceClient())
+                {
+
+                    client.Open();
+                    
+                    ServiceAgence.CriteresRechercheBiensImmobiliers a = new ServiceAgence.CriteresRechercheBiensImmobiliers();
+                   a.TitreContient = "o";
+                   a.AdresseContient = "";
+                   a.CodePostal = "";
+                   a.DateMiseEnTransaction1 = null;
+                   a.DateMiseEnTransaction2 = null;
+                   a.DateTransaction1 = null;
+                   a.DateTransaction2 = null;
+                   a.DescriptionContient = "";
+                   a.EnergieChauffage = null;
+                   a.MontantCharges1 = -1;
+                   a.MontantCharges2 = -1;
+                   a.NbEtages1 = -1;
+                   a.NbEtages2 = -1;
+                   a.NbPieces1 = -1;
+                   a.NbPieces2 = -1;
+                   a.NumEtage1 = -1;
+                   a.NumEtage2 = -1;
+                   a.Prix1 = -1;
+                   a.Prix2 = -1;
+                   a.Surface1 = -1;
+                   a.Surface2 = -1;
+                   a.TransactionEffectuee = null;
+                   a.Tris = null;
+                   a.TypeBien = null;
+                   a.TypeChauffage = null;
+                   a.TypeTransaction = null;
+                   a.Ville = "";
+
+                    ServiceAgence.ResultatListeBiensImmobiliers res = client.LireListeBiensImmobiliers(a, 0, 2);
+
+                    Label1.Text = res.Liste.List.First().Titre;
+                    
+                    client.Close();
+                }
+            }
+        }
+    }
+
+
+}
