1d2b364786c8314cda8eb653c5a98c0f99079386
Antloup <anthony.loup@etu.univ-lyon1.fr>
2016-03-08 14:33:13 +0100

    Ajouter admin rights

--- ClientWeb/Ajouter.aspx.cs
+++ ClientWeb/Ajouter.aspx.cs
@@ -12,121 +12,129 @@ namespace ClientWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Catalogue obj = new Catalogue();
 
-			string id = Request.QueryString["id"];
-
-            if (!this.IsPostBack)
+            if (Session["Admin"] == null || Session["Admin"].ToString() == "false")
             {
-
-                obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eTypeBien>(DropDownListTypeBien, false);
-                obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eTypeChauffage>(DropDownListTypeChauffage, false);
-                obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eEnergieChauffage>(DropDownListEnergieChauffage, false);
-                obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eTypeTransaction>(DropDownListTypeTransaction, false);
-
-                DropDownListTypeBien.SelectedValue = "0";
-                DropDownListEnergieChauffage.SelectedValue = "0";
-                DropDownListTypeChauffage.SelectedValue = "0";
-                DropDownListTypeTransaction.SelectedValue = "0";
-
-				
-
-				if (id != null)
-				{
-
-					using (ServiceAgence.AgenceClient client = new ServiceAgence.AgenceClient())
-					{
-						client.Open();
-
-						ServiceAgence.BienImmobilier bien = client.LireDetailsBienImmobilier(id).Bien;
-
-						Titre.Text = bien.Titre;
-						Prix.Text = bien.Prix.ToString();
-						MontantCharges.Text = bien.MontantCharges.ToString();
-						Adresse.Text = bien.Adresse;
-						CP.Text = bien.CodePostal;
-						Ville.Text = bien.Ville;
-						Surface.Text = bien.Surface.ToString();
-						NombreEtage.Text = bien.NbEtages.ToString();
-						NumeroEtage.Text = bien.NumEtage.ToString();
-						Description.Text = bien.Description;
-						NombrePiece.Text = bien.NbPieces.ToString();
-						Description.Text = bien.Description;
-						DropDownListTypeBien.SelectedIndex = (int)bien.TypeBien;
-						DropDownListTypeTransaction.SelectedIndex = (int)bien.TypeTransaction;
-						DropDownListTypeChauffage.SelectedIndex = (int)bien.TypeChauffage;
-						DropDownListEnergieChauffage.SelectedIndex = (int)bien.EnergieChauffage;
-
-					}
-
-				}
-
+                Response.Redirect("~/Connexion.aspx");
             }
-
-            if (this.IsPostBack)
+            else
             {
-                using (ServiceAgence.AgenceClient client = new ServiceAgence.AgenceClient())
+                Catalogue obj = new Catalogue();
+
+                string id = Request.QueryString["id"];
+
+                if (!this.IsPostBack)
                 {
-					client.Open();
 
-					ServiceAgence.BienImmobilier bien;
+                    obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eTypeBien>(DropDownListTypeBien, false);
+                    obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eTypeChauffage>(DropDownListTypeChauffage, false);
+                    obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eEnergieChauffage>(DropDownListEnergieChauffage, false);
+                    obj.Load_DropDownListItem<ServiceAgence.BienImmobilierBase.eTypeTransaction>(DropDownListTypeTransaction, false);
 
-					if (id == null)
-					{
-						bien = new ServiceAgence.BienImmobilier();
-						Initbien(bien);
-					}
-					else
-					{
-						bien = client.LireDetailsBienImmobilier(id).Bien;
-
-					}
+                    DropDownListTypeBien.SelectedValue = "0";
+                    DropDownListEnergieChauffage.SelectedValue = "0";
+                    DropDownListTypeChauffage.SelectedValue = "0";
+                    DropDownListTypeTransaction.SelectedValue = "0";
 
 
 
-                    bien.Adresse = Adresse.Text;
-                    bien.CodePostal = CP.Text;
-                    bien.DateMiseEnTransaction = DateTime.Now;
-                    bien.DateTransaction = null; // ?? A completer ??
-                    bien.Description = Description.Text;
-                    bien.EnergieChauffage = (ServiceAgence.BienImmobilierBase.eEnergieChauffage)obj.AffectSelectedValue(DropDownListEnergieChauffage);
-                    bien.MontantCharges = ConvertStringToDouble(MontantCharges.Text, 0);
-                    bien.NbEtages = ConvertStringToInt(NombreEtage.Text, 0);
-                    bien.NbPieces = ConvertStringToInt(NombrePiece.Text, 0);
-                    bien.NumEtage = ConvertStringToInt(NumeroEtage.Text, 0);
-                    bien.PhotoPrincipaleBase64 = "";
+                    if (id != null)
+                    {
 
-                    PutImage(ImageP, bien);
-                    PutImage(Image1, bien);
-                    PutImage(Image2, bien);
-                    PutImage(Image3, bien);
+                        using (ServiceAgence.AgenceClient client = new ServiceAgence.AgenceClient())
+                        {
+                            client.Open();
 
+                            ServiceAgence.BienImmobilier bien = client.LireDetailsBienImmobilier(id).Bien;
 
-                    bien.Prix = ConvertStringToDouble(Prix.Text, 0);
-                    bien.Surface = ConvertStringToDouble(Surface.Text, 0);
+                            Titre.Text = bien.Titre;
+                            Prix.Text = bien.Prix.ToString();
+                            MontantCharges.Text = bien.MontantCharges.ToString();
+                            Adresse.Text = bien.Adresse;
+                            CP.Text = bien.CodePostal;
+                            Ville.Text = bien.Ville;
+                            Surface.Text = bien.Surface.ToString();
+                            NombreEtage.Text = bien.NbEtages.ToString();
+                            NumeroEtage.Text = bien.NumEtage.ToString();
+                            Description.Text = bien.Description;
+                            NombrePiece.Text = bien.NbPieces.ToString();
+                            Description.Text = bien.Description;
+                            DropDownListTypeBien.SelectedIndex = (int)bien.TypeBien;
+                            DropDownListTypeTransaction.SelectedIndex = (int)bien.TypeTransaction;
+                            DropDownListTypeChauffage.SelectedIndex = (int)bien.TypeChauffage;
+                            DropDownListEnergieChauffage.SelectedIndex = (int)bien.EnergieChauffage;
 
-                    bien.Titre = Titre.Text;
-                    bien.TransactionEffectuee = false;
-                    bien.TypeBien = (ServiceAgence.BienImmobilierBase.eTypeBien)obj.AffectSelectedValue(DropDownListTypeBien);
-                    bien.TypeChauffage = (ServiceAgence.BienImmobilierBase.eTypeChauffage)obj.AffectSelectedValue(DropDownListTypeChauffage);
-                    bien.TypeTransaction = (ServiceAgence.BienImmobilierBase.eTypeTransaction)obj.AffectSelectedValue(DropDownListTypeTransaction);
-                    bien.Ville = Ville.Text;
+                        }
 
-					if (id != null)
-					{
-						bien.Id = Convert.ToInt32(id);
-						client.ModifierBienImmobilier(bien);
-					}
-					else
-					{
-						client.AjouterBienImmobilier(bien);
-					}
-                   
+                    }
 
-					client.Close();
                 }
 
-                Response.Redirect("~/Administration.aspx");
+                if (this.IsPostBack)
+                {
+                    using (ServiceAgence.AgenceClient client = new ServiceAgence.AgenceClient())
+                    {
+                        client.Open();
+
+                        ServiceAgence.BienImmobilier bien;
+
+                        if (id == null)
+                        {
+                            bien = new ServiceAgence.BienImmobilier();
+                            Initbien(bien);
+                        }
+                        else
+                        {
+                            bien = client.LireDetailsBienImmobilier(id).Bien;
+
+                        }
+
+
+
+                        bien.Adresse = Adresse.Text;
+                        bien.CodePostal = CP.Text;
+                        bien.DateMiseEnTransaction = DateTime.Now;
+                        bien.DateTransaction = null; // ?? A completer ??
+                        bien.Description = Description.Text;
+                        bien.EnergieChauffage = (ServiceAgence.BienImmobilierBase.eEnergieChauffage)obj.AffectSelectedValue(DropDownListEnergieChauffage);
+                        bien.MontantCharges = ConvertStringToDouble(MontantCharges.Text, 0);
+                        bien.NbEtages = ConvertStringToInt(NombreEtage.Text, 0);
+                        bien.NbPieces = ConvertStringToInt(NombrePiece.Text, 0);
+                        bien.NumEtage = ConvertStringToInt(NumeroEtage.Text, 0);
+                        bien.PhotoPrincipaleBase64 = "";
+
+                        PutImage(ImageP, bien);
+                        PutImage(Image1, bien);
+                        PutImage(Image2, bien);
+                        PutImage(Image3, bien);
+
+
+                        bien.Prix = ConvertStringToDouble(Prix.Text, 0);
+                        bien.Surface = ConvertStringToDouble(Surface.Text, 0);
+
+                        bien.Titre = Titre.Text;
+                        bien.TransactionEffectuee = false;
+                        bien.TypeBien = (ServiceAgence.BienImmobilierBase.eTypeBien)obj.AffectSelectedValue(DropDownListTypeBien);
+                        bien.TypeChauffage = (ServiceAgence.BienImmobilierBase.eTypeChauffage)obj.AffectSelectedValue(DropDownListTypeChauffage);
+                        bien.TypeTransaction = (ServiceAgence.BienImmobilierBase.eTypeTransaction)obj.AffectSelectedValue(DropDownListTypeTransaction);
+                        bien.Ville = Ville.Text;
+
+                        if (id != null)
+                        {
+                            bien.Id = Convert.ToInt32(id);
+                            client.ModifierBienImmobilier(bien);
+                        }
+                        else
+                        {
+                            client.AjouterBienImmobilier(bien);
+                        }
+
+
+                        client.Close();
+                    }
+
+                    Response.Redirect("~/Administration.aspx");
+                }
             }
         }
 
